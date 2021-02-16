f0eb96f714d88497a4a5bf0da92055e425096537
Matt Randolph <mttrndlph@gmail.com>
2021-02-16 13:43:46 -0500

    Added manager inquirer prompts

--- index.js
+++ index.js
@@ -6,3 +6,44 @@ const Employee = require("./lib/employee");
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
+
+managementArray = [];
+
+//Prompts for managers, pushes to managementArray
+
+function managerInput() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please input manager name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Please input manager email address",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Please input manager id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Please input manager office number",
+        name: "office",
+      },
+    ])
+    .then((managerResponse) => {
+      const manager = new Manager(
+        managerResponse.name,
+        managerResponse.email,
+        managerResponse.id,
+        managerResponse.office
+      );
+      managementArray.push(manager);
+    });
+}
+
+managerInput();
